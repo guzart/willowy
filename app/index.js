fd109a77c770320c1ad31e1f0e6b0c6d5a1311d1
Arturo Guzman <arturo@guzart.com>
2016-11-30 00:37:39 -0500

    chore(app): update data defaults

--- app/index.js
+++ app/index.js
@@ -5,9 +5,9 @@ import App from './components/App.svelt'
 
 const app = new App({
   target: global.document.getElementById('main'),
-  data: { name: 'world' },
+  data: { name: 'everybody' },
 })
 
-app.set({ name: 'everybody' })
+app.set({ name: 'nobody' })
 
 // app.teardown()
